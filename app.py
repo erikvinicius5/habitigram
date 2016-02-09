1089e7b72c7a410d4195465508609ae7ec1fe962
Erik Vinicius <erikvinicius5@gmail.com>
2016-02-09 19:19:39 -0300

    Add new endpoint to set webhook

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, url_for, request
+import os
+import requests
+
 app = Flask(__name__)
+
+
+def generate_telegram_method_url(method):
+    return 'https://api.telegram.org/bot{token}/{method}'\
+        .format(token=os.environ.get('TOKEN'), method=method)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello"
+
+@app.route("/%s" % os.environ.get('ENCRYPTED_TOKEN'), methods=["POST"])
+def post_update():
+    print request.form
+
+@app.route("/toggle_hook")
+def toggle_hook():
+    telegram_url = generate_telegram_method_url('setWebhook')
+    certificate_path = '/static/certificates/public.pem'
+    payload = { 'url': url_for(os.environ.get('ENCRYPTED_TOKEN')) }
+    requests.post(telegram_url, data=payload, cert=certificate_path)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
