c28e80c61611a73bab2f5d04de0a09c3d60b5eed
Erik Vinicius <erikvinicius5@gmail.com>
2016-02-10 14:20:13 -0300

    Enable setWebHook

--- app.py
+++ app.py
@@ -3,27 +3,29 @@ import os
 import requests
 
 app = Flask(__name__)
+app.config['SERVER_NAME'] = os.environ.get('HEROKU_URL', '')
+app.config['PREFERRED_URL_SCHEME'] = 'https'
 
 
 def generate_telegram_method_url(method):
     return 'https://api.telegram.org/bot{token}/{method}'\
-        .format(token=os.environ.get('TOKEN'), method=method)
+        .format(token=os.environ.get('TOKEN', ''), method=method)
 
 @app.route("/")
 def hello():
     return "Hello"
 
-@app.route("/%s" % os.environ.get('ENCRYPTED_TOKEN'), methods=["POST"])
+@app.route("/%s" % os.environ.get('ENCRYPTED_TOKEN', ''), methods=["POST"])
 def post_update():
     print request.form
 
 @app.route("/toggle_hook")
 def toggle_hook():
     telegram_url = generate_telegram_method_url('setWebhook')
-    certificate_path = '/static/certificates/public.pem'
-    payload = { 'url': url_for(os.environ.get('ENCRYPTED_TOKEN')) }
-    requests.post(telegram_url, data=payload, cert=certificate_path)
+    payload = { 'url': url_for('post_update') }
+    req = requests.post(telegram_url, data=payload)
+    return req.text
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
